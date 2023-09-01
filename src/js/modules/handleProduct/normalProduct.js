dd436d1d4bb6ab114e7494377eec3974c41008c0
deigo-lps <diego.lopes0909@gmail.com>
2023-09-01 15:10:38 -0300

    refaz camiseta

--- src/js/modules/handleProduct/normalProduct.js
+++ src/js/modules/handleProduct/normalProduct.js
@@ -15,7 +15,7 @@ const normalProduct = (product) => {
   const hasImg = currentRow.classList.contains("has-img") || currentRow.classList.contains("has-img-desktop")
   const [variantsWrapper, dropdownMobile, dropdownImg] = createVariantsWrapper(currentRow, product.variants, hasImg)
   product.variants.forEach((variant) => {
-    const [wrapper, button] = createButton(product.id, variant.id, variant.title, hasImg, variant.image.src, variant.price.amount)
+    const [wrapper, button] = createButton({productId: product.id, variantId: variant.id, text: variant.title, hasImg: hasImg, src: variant.image.src, variantPrice: variant.price.amount})
 
     if (currentRow.classList.contains("move-last-variant") && variant["last-variant"]) {
       document.querySelector(`.last-variant.prod-${product.id}`).appendChild(wrapper)
