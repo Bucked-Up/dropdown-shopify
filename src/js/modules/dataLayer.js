dfbde079f2eb046b4e6e862245612eb07c1027a4
deigo-lps <diego.lopes0909@gmail.com>
2023-12-08 17:53:09 -0300

    adds more info to klaviyo

--- src/js/modules/dataLayer.js
+++ src/js/modules/dataLayer.js
@@ -20,14 +20,14 @@ const dataLayerStart = (data) => {
   const titles = data.map((items) => items.title);
   const item = { event: "pageview", action: "load", value: 0 };
   setDataLayer(item);
-  klaviyo.push(["track", "Page View", { ...obj, ...item, products: titles }]);
+  klaviyo.push(["track", "Page View", { ...obj, ...item, products: titles, pagepath: window.location.pathname, pageurl: window.location.href, time: currentTime.getTime() }]);
 };
 
 const dataLayerRedirect = (data) => {
   const titles = data.map((items) => items.title);
   const item = { event: "offerview", action: "viewaction", value: 0 };
   setDataLayer(item);
-  klaviyo.push(["track", "User Redirect Engagement", { ...obj, ...item, products: titles }]);
+  klaviyo.push(["track", "User Redirect Engagement", { ...obj, ...item, products: titles, pagepath: window.location.pathname, pageurl: window.location.href, time: currentTime.getTime() }]);
 };
 
 export { dataLayerStart, dataLayerRedirect };
