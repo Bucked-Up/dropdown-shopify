832409ea82d52add09402bb87031fa91690aceba
deigo-lps <diego.lopes0909@gmail.com>
2024-01-16 16:57:30 -0300

    adds klaviyo and cookies

--- src/js/instapage/script.js
+++ src/js/instapage/script.js
@@ -1,14 +1,9 @@
-const urlParams = new URLSearchParams(window.location.search);
-const origin = window.location.pathname.replace("/", "").replace("/", "");
-document.cookie = "offer_id=" + origin + "; path=/; domain=.buckedup.com;max-age=3600";
-localStorage.setItem("first_page", origin);
-
-const utm_source = "";
 const step_count = "";
 const page_id = "";
 const version_id = "";
+const urlParamsCookies = ["click_id", "utm_source", "utm_medium", "utm_campaign", "utm_term", "utm_content"];
 
-const productsID = [8685150306610]; //ID of each the product
+const productsID = [8685147062578]; //ID of each the product
 const hiddenProducts = []
 const optionalProducts = []
 const isKit = true;
@@ -17,6 +12,15 @@ const discountCode = ""
 const lastVariantElements = ["#test1", "#test2"] //for when last variant is moved.
 
 //stop here.
+const urlParams = new URLSearchParams(window.location.search);
+const origin = window.location.pathname.replace("/", "").replace("/", "");
+const cookieConfig = "path=/; domain=.buckedup.com;max-age=3600";
+document.cookie = `offer_id=${discountCode};${cookieConfig}`;
+document.cookie = `page_id=${page_id};${cookieConfig}`;
+urlParamsCookies.forEach((param) => {
+  document.cookie = `${param}=${urlParams.get(param)};${cookieConfig}`;
+});
+localStorage.setItem("first_page", origin);
 
 const buyButton = [];
 const row = {};
